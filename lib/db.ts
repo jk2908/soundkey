0e85d158804607748f27f948157c563ddeed7854
Jerome Kenway <jerome@digitalmarmalade.co.uk>
2024-02-13 09:25:30 +0000

    Add pkgs

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,6 @@
-import { neon, neonConfig, Pool } from '@neondatabase/serverless'
-import { drizzle } from 'drizzle-orm/neon-http'
+import Database from 'better-sqlite3'
+import { drizzle } from 'drizzle-orm/better-sqlite3'
 
 import * as schema from '@/lib/schema'
 
-neonConfig.fetchConnectionCache = true
-
-export const pool = new Pool({ connectionString: process.env.DB_URL })
-export const sql = neon(process.env.DB_URL ?? '')
-export const db = drizzle(sql, { schema })
+export const db = new Database(process.env.DB_URL)
