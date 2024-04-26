d7fc334eb22815e77acee28f5e43ecb698c55298
Jerome Kenway <jerome@digitalmarmalade.co.uk>
2024-04-26 21:19:25 +0100

    Remove aspect 1 / 1 from iconOnly buttons

--- components/global/button.tsx
+++ components/global/button.tsx
@@ -69,7 +69,7 @@ export const Button = forwardRef(
         className={cn(
           'flex items-center gap-3 rounded-full px-8 py-2',
           styleMap[variant],
-          iconOnly && 'aspect-square p-2',
+          iconOnly && 'p-2',
           className
         )}
         {...rest}>
