e8223b0f0cc6ddcb80936b29cbd7d34489ed8919
Jerome Kenway <jerome@digitalmarmalade.co.uk>
2024-04-26 21:10:22 +0100

    Update to React 18.3

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
-import MillionLint from '@million/lint'
-
 const config = {
   reactStrictMode: true,
-  experimental: {
-    //ppr: true,
-  },
   webpack: config => {
     config.externals.push('@node-rs/argon2', '@node-rs/bcrypt')
     return config
@@ -12,7 +7,3 @@ const config = {
 }
 
 export default config
-/*
-export default MillionLint.next({
-  rsc: true
-})(config);*/
